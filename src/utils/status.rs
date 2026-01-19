e9834f5cd97d587abb3bedd8c734cef96342f600
JanssenAaron <110543972+JanssenAaron@users.noreply.github.com>
2026-01-19 16:27:24 -0600

    Added utils directory

---
+++ src/utils/status.rs
@@ -0,0 +1,69 @@
+use chrono::Utc;
+use k8s_openapi::apimachinery::pkg::apis::meta::v1::{Condition, Time};
+use serde_json::Value;
+
+fn status_patch(conditions: Vec<Condition>) -> Value {
+    serde_json::json!({
+            "status": {
+                "conditions": conditions
+    }
+    })
+}
+
+// PUN Controller conditions
+pub struct PUNConditions {
+    deployment: Condition,
+    ready: Condition,
+}
+
+impl PUNConditions {
+    pub fn new() -> Self {
+        PUNConditions {
+            deployment: Condition {
+                status: "Unknown".to_string(),
+                type_: "DeploymentReady".to_string(),
+                last_transition_time: Time(Utc::now()),
+                message: String::new(),
+                observed_generation: None,
+                reason: String::new(),
+            },
+            ready: Condition {
+                status: "Unknown".to_string(),
+                type_: "Ready".to_string(),
+                last_transition_time: Time(Utc::now()),
+                message: String::new(),
+                observed_generation: None,
+                reason: String::new(),
+            },
+        }
+    }
+
+    pub fn deployment(&mut self, generation: Option<i64>, message: String, status: String) {
+        self.deployment = Condition {
+            last_transition_time: Time(Utc::now()),
+            message,
+            observed_generation: generation,
+            reason: String::new(),
+            status,
+            type_: format!("DeploymentReady"),
+        }
+    }
+    pub fn ready(&mut self, generation: Option<i64>, message: String, status: String) {
+        self.ready = Condition {
+            last_transition_time: Time(Utc::now()),
+            message,
+            observed_generation: generation,
+            reason: String::new(),
+            status,
+            type_: format!("Ready"),
+        }
+    }
+
+    pub fn get_patch(self) -> Value {
+        let mut conditions = vec![];
+        conditions.push(self.deployment);
+        conditions.push(self.ready);
+
+        status_patch(conditions)
+    }
+}
