4bb9d2d59de5d73d08b914dc8a998358f85ded79
JanssenAaron <110543972+JanssenAaron@users.noreply.github.com>
2026-03-06 08:33:34 -0600

    Added cleanup step to container image pun_entry.sh

--- operator-image/pun_entry.sh
+++ operator-image/pun_entry.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/bash
+
+cleanup() {
+    /opt/ood/nginx_stage/sbin/nginx_stage nginx --user $1 -s stop
+    echo STOP CALLED
+    exit 0
+}
+
+trap 'cleanup' TERM INT
+
 /opt/ood/nginx_stage/sbin/nginx_stage pun -u $1
-sleep 9000000
+sleep 9000000 &
+wait
